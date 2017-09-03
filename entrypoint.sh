73dcea3926c5c1c12ee34f3e6de7be0e7ab1b349
Patrick Eichmann <phreakazoid@phreakazoid.com>
2017-09-03 10:40:03 +0200

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+
+if ! [ "$(ls -A /etc/asterisk)" ]; then
+	cp -R /etc/asterisk.config/* /etc/asterisk/
+fi
+
+if ! [ "$(ls -A /var/lib/asterisk)" ]; then
+	cp -R /var/lib/asterisk.data/* /var/lib/asterisk/
+fi
+
+service asterisk start
+tail -f /var/log/asterisk/messages
